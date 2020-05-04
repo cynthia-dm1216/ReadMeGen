41c2f95c6579d4c27f39bce52b0624387afcef8e
Cynthia Dominguez <cynthia.dm1216@gmail.com>
2020-05-04 16:57:53 -0500

    worked on utils folder

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,35 @@
+function generateMarkdown(answers, response) {
+    return `
+    # ${answers.title}
+
+    ![badge](${answers.badge})
+
+    ## Description
+    ${answers.descripton}
+
+    ## Table of Contents
+    ${answers.contents.join('\n')}
+
+    ### Installation
+    ${answers.installation}
+
+    ### Usage
+    ${answers.usage}
+
+    ### Credits
+    ${answers.credits}
+
+    ### License
+    ${answers.license}
+
+    ### Tests
+    ${answers.tests}
+
+    ### Questions
+    ![GitHub profile image](${response.data.avatar_url}=100x100)\n
+    email= ${response.data.email}
+
+    `;
+}
+
+module.exports = generateMarkdown;
